d81da33201839e6b84d9cb03e2449d134c288771
Yohann Andre <y.andre90000@gmail.com>
2023-10-27 15:49:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {  // Chargement du dom 
+    console.log('Document Loaded');
+
+    document.querySelector('#contactForm')
+        .addEventListener("submit", async (e) => {
+            e.preventDefault() // suppression du comportement par defaut de l'evenement submit
+
+            const data = {
+                firstname: document.querySelector("#firstname").value,
+                lastname: document.querySelector("#lastname").value,
+                email: document.querySelector("#email").value,
+                subject: document.querySelector("#subject").value,
+                message: document.querySelector("#message").value,
+            };
+
+            // console.log(data)
+
+            const response = await axios.post('http://localhost:3000/form', data) // ici je définis la route sur laquelle je veux envoyer mon formulaire et en deuxième argument je dis ce que je veux envoyer
+            console.log(response)
+        })
+})
